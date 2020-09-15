a313cf861055ddebb8675efa622f37d7cc9fe266
Ruslan Khasbatov <ruslan.khasbatov@x5.ru>
2020-09-15 18:45:35 +0300

    ALF#3 Добавил все отставшиеся API для сервисов списание, пополнения и активации

--- db-service/src/main/java/ru/khasbatov/dbservice/controllers/DbController.java
+++ db-service/src/main/java/ru/khasbatov/dbservice/controllers/DbController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DbController {
+public class DbController  {
 
     @GetMapping("/get")
     public ResponseEntity<String> hello() {
